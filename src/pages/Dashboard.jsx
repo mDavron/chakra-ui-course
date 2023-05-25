5eaf46b6179c5e66478809c570b8d2f31369eba9
Davron01~ <d.mirabdullayev@mail.ru>
2023-05-25 21:37:37 +0500

    chakra-ui course start

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { Heading, Text, Container } from "@chakra-ui/react";
+export default function Dashboard() {
+  return (
+    <Container>
+      <Heading>Chakra ui Components</Heading>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis eius
+      </Text>
+    </Container>
+  );
+}
