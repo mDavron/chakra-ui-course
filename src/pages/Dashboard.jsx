839b0c08b489e2f75ad8c202c6851ec28a598a3e
Davron01~ <d.mirabdullayev@mail.ru>
2023-05-26 10:04:23 +0500

    chakra ui grid

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,35 +1,23 @@
-import { Heading, Text, Container, Box } from "@chakra-ui/react";
+import React from "react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
+
 export default function Dashboard() {
-  const boxStyles = {
-    p: "10px",
-    bg: "purple.400",
-    color: "white",
-    m: "10px",
-    textAlign: "center",
-    ":hover": {
-      color: "#000",
-      bg: "blue.200",
-    },
-  };
   return (
-    <Container as="section" maxWidth="6xl">
-      <Heading my="30px" p="10px">
-        Chakra ui Components
-      </Heading>
-      <Text ml="30px">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. dolor sit amet
-        Officiis eius?
-      </Text>
-      <Text marginLeft="30px" color="blue.800" fontWeight="bold">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. dolor sit amet
-        Officiis eius
-      </Text>
-      <Box my="30px" p="20px" bg="orange">
-        <Text color="white">This is a Box</Text>
-      </Box>
-      <Box sx={boxStyles}>
-        <Text>Hello Chakra Ui!</Text>
-      </Box>
-    </Container>
+    <SimpleGrid p="10px" spacing={10} minChildWidth="250px">
+      <Box bg="white" h="200" border="1px solid"></Box>
+      <Box bg="white" h="200" border="1px solid"></Box>
+      <Box bg="white" h="200" border="1px solid"></Box>
+      <Box bg="white" h="200" border="1px solid"></Box>
+
+      <Box bg="white" h="200" border="1px solid"></Box>
+      <Box bg="white" h="200" border="1px solid"></Box>
+      <Box bg="white" h="200" border="1px solid"></Box>
+      <Box bg="white" h="200" border="1px solid"></Box>
+
+      <Box bg="white" h="200" border="1px solid"></Box>
+      <Box bg="white" h="200" border="1px solid"></Box>
+      <Box bg="white" h="200" border="1px solid"></Box>
+      <Box bg="white" h="200" border="1px solid"></Box>
+    </SimpleGrid>
   );
 }
