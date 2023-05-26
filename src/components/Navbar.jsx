2473e97359cf1337ef8c89ce3e88f0f6eb1e2985
Davron01~ <d.mirabdullayev@mail.ru>
2023-05-26 22:46:35 +0500

    json-server,card components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ import {
 
 export default function Navbar() {
   return (
-    <Flex as="nav" p="10" alignItems="center">
+    <Flex as="nav" p="10" alignItems="center" mb="40px">
       <Heading as="h1">Davron Tasks</Heading>
       <Spacer />
       <HStack spacing="20px">
@@ -22,19 +22,5 @@ export default function Navbar() {
         <Button colorScheme="purple">Logout</Button>
       </HStack>
     </Flex>
-    // <Flex bg="gray.200" justify="space-around" wrap="wrap" gap="2">
-    //   <Box w="150px" h="50px" bg="red">
-    //     1
-    //   </Box>
-    //   <Box w="150px" h="50px" bg="blue">
-    //     2
-    //   </Box>
-    //   <Box w="150px" h="50px" bg="yellow" flexGrow={1}>
-    //     3
-    //   </Box>
-    //   <Box w="150px" h="50px" bg="green" flexGrow={2}>
-    //     4
-    //   </Box>
-    // </Flex>
   );
 }
